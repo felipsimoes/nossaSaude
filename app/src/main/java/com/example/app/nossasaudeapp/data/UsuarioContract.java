27fa45a9295131afb50716eaf42a3d06b609367a
Felipe Simões <felipe_lima_simoes@hotmail.com>
2017-03-08 17:12:26 -0300

    optimizing gradle time to compile

---
+++ app/src/main/java/com/example/app/nossasaudeapp/data/UsuarioContract.java
@@ -0,0 +1,12 @@
+package com.example.app.nossasaudeapp.data;
+
+/**
+ * Created by Felipe on 07/03/2017.
+ */
+
+public class UsuarioContract {
+
+    private UsuarioContract() { }
+
+    private static final String CONTENT_AUTHORITY = "com.example.app.nossasaude";
+}
