44292779738e31600806c4f03bf48bf1c61a2a2b
Felipe Simões <felipe_lima_simoes@hotmail.com>
2017-04-08 16:41:15 -0300

    alarm most simple working version

--- app/src/main/java/com/example/app/nossasaudeapp/AlarmReceiver.java
+++ app/src/main/java/com/example/app/nossasaudeapp/AlarmReceiver.java
@@ -1,8 +1,13 @@
 package com.example.app.nossasaudeapp;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 /**
@@ -10,9 +15,14 @@ import android.util.Log;
  */
 
 public class AlarmReceiver extends BroadcastReceiver {
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.e("Receiver","In the receiver");
+//        Log.d("Receiver","In the receiver");
+        Intent intentService = new Intent(context, AlarmService.class);
 
+        context.startService(intentService);
     }
+
+
 }
---
+++ app/src/main/java/com/example/app/nossasaudeapp/AlarmService.java
@@ -0,0 +1,55 @@
+package com.example.app.nossasaudeapp;
+
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Build;
+import android.os.IBinder;
+import android.support.annotation.IntDef;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+/**
+ * Created by Felipe on 04/04/2017.
+ */
+
+public class AlarmService extends Service {
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.d("MyActivity", "In the service");
+        return null;
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        Log.d("HERE", "ON START");
+        final NotificationManager mNM = (NotificationManager)
+                getSystemService(NOTIFICATION_SERVICE);
+
+        Intent intent1 = new Intent(this.getApplicationContext(), MedicamentoActivity.class);
+        PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent1, 0);
+
+        Notification mNotify  = new Notification.Builder(this)
+                .setContentTitle("TITLE")
+                .setContentText("Click me!")
+                .setSmallIcon(R.drawable.notification_template_icon_bg)
+                .setContentIntent(pIntent)
+                .setAutoCancel(true)
+                .build();
+
+        mNM.notify(0, mNotify);
+        Log.d("HERE", "NOTIFICACAO SETADA");
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
