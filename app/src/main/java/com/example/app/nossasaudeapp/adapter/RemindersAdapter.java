953be710200b1cb2ebecc1588cbf0c137165c675
Felipe Simões <felipe_lima_simoes@hotmail.com>
2017-06-08 13:44:20 -0300

    Link para os itens a partir da tela layout

--- app/src/main/java/com/example/app/nossasaudeapp/adapter/RemindersAdapter.java
+++ app/src/main/java/com/example/app/nossasaudeapp/adapter/RemindersAdapter.java
@@ -1,5 +1,8 @@
 package com.example.app.nossasaudeapp.adapter;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +12,10 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.app.nossasaudeapp.R;
+import com.example.app.nossasaudeapp.activities.ConsultaViewActivity;
+import com.example.app.nossasaudeapp.activities.ExameViewActivity;
+import com.example.app.nossasaudeapp.activities.MedicamentoActivity;
+import com.example.app.nossasaudeapp.activities.MedicamentoViewActivity;
 import com.example.app.nossasaudeapp.data.Consulta;
 import com.example.app.nossasaudeapp.data.Exame;
 import com.example.app.nossasaudeapp.data.Medicamento;
@@ -79,20 +86,44 @@ public class RemindersAdapter extends RecyclerView.Adapter<RemindersAdapter.MyVi
         RealmObject realmObject = list.get(position);
 
         if (realmObject instanceof Medicamento) {
-            Medicamento medicamento = (Medicamento) realmObject;
+            final Medicamento medicamento = (Medicamento) realmObject;
             holder.linearItem.setBackgroundResource(R.color.colorMedicamento);
             holder.name.setText(medicamento.getNome());
             holder.iconeReminder.setImageResource(R.mipmap.remedio);
+            holder.linearItem.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(v.getContext(), MedicamentoViewActivity.class);
+                    intent.putExtra("NOTIFICATION_ID", medicamento.getId());
+                    v.getContext().startActivity(intent);
+                }
+            });
         } else if (realmObject instanceof Exame) {
-            Exame exame = (Exame) realmObject;
+            final Exame exame = (Exame) realmObject;
             holder.linearItem.setBackgroundResource(R.color.colorExame);
             holder.name.setText(exame.getNome());
             holder.iconeReminder.setImageResource(R.mipmap.exame);
+            holder.linearItem.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(v.getContext(), ExameViewActivity.class);
+                    intent.putExtra("NOTIFICATION_ID", exame.getId());
+                    v.getContext().startActivity(intent);
+                }
+            });
         } else if (realmObject instanceof Consulta) {
-            Consulta consulta = (Consulta) realmObject;
+            final Consulta consulta = (Consulta) realmObject;
             holder.linearItem.setBackgroundResource(R.color.colorConsulta);
             holder.name.setText(consulta.getNome());
             holder.iconeReminder.setImageResource(R.mipmap.consultas);
+            holder.linearItem.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(v.getContext(), ConsultaViewActivity.class);
+                    intent.putExtra("NOTIFICATION_ID", consulta.getId());
+                    v.getContext().startActivity(intent);
+                }
+            });
         }
     }
 
