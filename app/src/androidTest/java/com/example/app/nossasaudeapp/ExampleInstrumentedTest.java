bbfb8317768f1a5daf249f3ba1209324ca52c8c2
Felipe Lima Simões <felipe_lima_simoes@hotmail.com>
2017-11-13 08:56:44 -0200

    navegação botão voltar em exame e consulta, e validação em medicamento

--- app/src/androidTest/java/com/example/app/nossasaudeapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/app/nossasaudeapp/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.diegogaldino.nossasaudeapp", appContext.getPackageName());
+        assertEquals("com.example.app.nossasaudeapp", appContext.getPackageName());
     }
 }
